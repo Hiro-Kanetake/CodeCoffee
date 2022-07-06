63afd5cf839c859131296e3b3455ede9738cd32f
Hiro-Kanetake <git@github.com>
2022-07-06 23:03:04 +0900

    Add refactored function

--- evenOddSplitter.js
+++ evenOddSplitter.js
@@ -4,15 +4,26 @@ const evenOddSplitter = (arr) => {
   const odd = [];
   const even = [];
   const result = [odd, even];
-  // add the elements in the even indexes to result array as an array
   for (let i = 0; i < arr.length; i++) {
+    // add the elements in the odd indexes to result array as an array
     if (i % 2 === 0) {
       odd.push(arr[i]);
+    // add the elements in the even indexes to result array as an array
     } else if (i % 2 !== 0) {
       even.push(arr[i]);
     }
-    // add the elements in the odd indexes to result array as an array
   }
   // Return result array with 2 array elements
   return result;
 };
+
+// Refactored version;
+// const evenOddSplitter = (arr) => {
+//   // Create result array
+//   const result = [];
+//   // add the elements in the even indexes to result array as an array
+//   result.push(arr.filter((el, i) =>  (i % 2 === 0)));
+//   result.push(arr.filter((el, i) =>  (i % 2 !== 0)));
+//   // Return result array with 2 array elements
+//   return result;
+// };
